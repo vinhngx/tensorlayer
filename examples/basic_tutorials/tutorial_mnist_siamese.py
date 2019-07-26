c7bf93873389edcdb21e215e894e81e74c6375e4
Vinh Nguyen <vinh.nguyenx@gmail.com>
2019-07-26 14:13:00 +1000

    fix import: os

--- examples/basic_tutorials/tutorial_mnist_siamese.py
+++ examples/basic_tutorials/tutorial_mnist_siamese.py
@@ -12,6 +12,7 @@ For more details, see the reference paper.
 
 import random
 import time
+import os
 
 import numpy as np
 
@@ -98,7 +99,7 @@ te_pairs, te_y = create_pairs(X_test, digit_indices)
 print_freq = 5
 train_weights = model.trainable_weights
 optimizer = tf.optimizers.RMSprop()
-if os.environ.get('TF_ENABLE_AUTO_MIXED_PRECISION', default='0') == '1':
+if os.environ.get("TF_ENABLE_AUTO_MIXED_PRECISION", default="0") == "1":
     optimizer = tf.train.experimental.enable_mixed_precision_graph_rewrite(optimizer)
 
 @tf.function
